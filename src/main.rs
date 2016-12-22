04d21e362b24ee8266325cfcc984a6c9124b1c35
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-12-22 18:06:06 -0500

    Fix randable_enum to work with multiple invocations
    Before, the enum used a const, which meant it would not work for multiple uses. This changes it to a function.

--- src/main.rs
+++ src/main.rs
@@ -5,42 +5,56 @@ use rand::{Rand, Rng};
 fn main() {
     for _ in 0..10 {
         let note = Note::rand(&mut rand::thread_rng());
-        println!("{:?} {:?} {:?}", note.octave, note.letter, note.accidental);
+        println!("{:?} {:?} {:?}", note.octave, note.note.letter, note.note.accidental);
+        let signature = KeySignature::rand(&mut rand::thread_rng());
+        println!("{:?} {:?}", signature.mode, signature.key);
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct KeySignature {
+    mode: Mode,
+    key: NoteName,
+}
+
+impl Rand for KeySignature {
+    fn rand<R: Rng>(rng: &mut R) -> KeySignature {
+        KeySignature {
+            mode: Mode::rand(rng),
+            key: NoteName::rand(rng)
+        }
     }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Note {
     octave: i64,
+    note: NoteName,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct NoteName {
     letter: Letter,
-    accidental: Accidental,
+    accidental: Accidental
 }
 
 impl Rand for Note {
     fn rand<R: Rng>(rng: &mut R) -> Note {
         Note {
-            octave: rng.gen_range(-2, 8),
+            octave: rng.gen_range(-3, 8) as i64,
+            note: NoteName::rand(rng)
+        }
+    }
+}
+
+impl Rand for NoteName {
+    fn rand<R: Rng>(rng: &mut R) -> NoteName {
+        NoteName {
             letter: Rand::rand(rng),
             accidental: Rand::rand(rng),
         }
     }
 }
-
-// #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-// pub enum Letter {
-//     A,
-//     B,
-//     C,
-//     D,
-//     E,
-//     F,
-//     G,
-// }
-
-// const LETTER_VALUES: [Letter; 7] = {
-//     use Letter::*;
-//     [A, B, C, D, E, F, G]
-// };
 
 macro_rules! count {
     () => { 0 };
@@ -50,19 +64,22 @@ macro_rules! count {
 
 macro_rules! randable_enum {
     (pub enum $Name:ident { $($x:ident),* }) => {
+
         #[derive(Clone, Copy, Debug, PartialEq, Eq)]
         pub enum $Name {
             $($x),*
         }
 
-        const VALUES: [$Name; count!($($x),*)] = {
-            use $Name::*;
-            [$($x),*]
-        };
+        impl $Name {
+            fn values() -> [$Name; count!($($x),*)] {
+                use $Name::*;
+                [$($x),*]
+            }
+        }
 
         impl Rand for $Name {
-            fn rand<R: Rng>(rng: &mut R) -> Self {
-                VALUES[rng.gen_range(0, VALUES.len())]
+            fn rand<R: Rng>(rng: &mut R) -> $Name {
+                $Name::values()[rng.gen_range(0, $Name::values().len())]
             }
         }
     }
@@ -74,26 +91,17 @@ randable_enum! {
     }
 }
 
-// impl Rand for Letter {
-//     fn rand<R: Rng>(rng: &mut R) -> Letter {
-//         LETTER_VALUES[rng.gen_range(0, LETTER_VALUES.len())]
-//     }
-// }
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Accidental {
-    Sharp,
-    Flat,
-    Natural,
+randable_enum! {
+    pub enum Accidental {
+        Sharp,
+        Flat,
+        Natural
+    }
 }
 
-const ACCIDENTAL_VALUES: [Accidental; 3] = {
-    use Accidental::*;
-    [Sharp, Flat, Natural]
-};
-
-impl Rand for Accidental {
-    fn rand<R: Rng>(rng: &mut R) -> Accidental {
-        ACCIDENTAL_VALUES[rng.gen_range(0,  ACCIDENTAL_VALUES.len())]
+randable_enum! {
+    pub enum Mode {
+        Major,
+        Minor
     }
 }
